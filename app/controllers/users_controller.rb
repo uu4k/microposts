0263e9e89cd966919b658e6d7c17147f46bfa6ed
uu4k <t.shimizu1115@gmail.com>
2015-08-08 06:26:19 +0000

    create followers/followings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,16 @@ class UsersController < ApplicationController
     end
   end
   
+  def followings
+    @user = User.find(params[:id])
+    @following_users = @user.following_users
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @follower_users = @user.follower_users
+  end
+  
   private
   def user_params
     params.require(:user).permit(:name,:email,:password,:password_confirmation,:profile,:location)
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_many :following_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :following_users, through: :following_relationships, source: :followed
   
-  has_many :follower_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :follower_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :follower_users, through: :follower_relationships, source: :follower
   
   def follow(other_user)
